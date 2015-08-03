12a31f4a6ac686e56e3e5d066c41f7e4ff18847f
Paresh Radadiya <pareshradadiya@hotmail.com>
2015-08-04 01:57:11 +0530

    Uninstall script added

---
+++ uninstall.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * BuddyView Uninstall
+ *
+ * Uninstalling BuddyView deletes tables and user meta etc.
+ *
+ * @author      paresh
+ * @category    Core
+ * @package     BuddyView/Uninstaller
+ * @version     1.0
+ */
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+global $wpdb;
+
+
+// Tables
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}bbv_profile_view_log" );
